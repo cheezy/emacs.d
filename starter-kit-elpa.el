6541b53fdf4417487afd147cc7296335fa699870
Jeffrey S. Morgan <jeff@expectmoreinc.com>
2009-02-21 13:41:37 -0500

    first commit

---
+++ starter-kit-elpa.el
@@ -0,0 +1,62 @@
+;;; starter-kit-elpa.el --- Install a base set of packages automatically.
+;;
+;; Part of the Emacs Starter Kit
+
+(defvar starter-kit-packages (list 'idle-highlight
+                                   'ruby-mode
+                                   'inf-ruby
+                                   'js2-mode
+                                   'css-mode
+                                   'nxml
+                                   'gist
+                               ;; To submit
+;;;                                "magit"
+;;;                                "paredit"
+;;;                                "clojure-mode"
+;;;                                "yaml"
+;;;                                "haml"
+;;;                                "sass"
+;;;                                "cheat"
+;;;                                "html-fontify"
+;;;                                "color-theme"
+;;;                                "color-theme-zenburn"
+;;;                                "color-theme-vivid-chalk"
+                               ;; Complicated ones
+;;;                                "nxhtml"
+;;;                                "rinari"
+;;;                                "jabber"
+;;;                                "slime"
+;;;                                "swank-clojure"
+                                   )
+  "Libraries that should be installed by default.")
+
+;; Work around a bug in ELPA
+(ignore-errors (load "elpa/inf-ruby-2.0/inf-ruby-autoloads"))
+(ignore-errors (load "elpa/lisppaste-1.5/lisppaste-autoloads"))
+
+(defun starter-kit-elpa-install ()
+  "Install all starter-kit packages that aren't installed."
+  (interactive)
+  (dolist (package starter-kit-packages)
+    (unless (or (member package package-activated-list)
+                (functionp package))
+      (message "Installing %s" (symbol-name package))
+      (package-install package))))
+
+(defun esk-online? ()
+  "See if we're online.
+
+Windows does not have the network-interface-list function, so we
+just have to assume it's online."
+  (if (and (functionp 'network-interface-list)
+           (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+                         (member 'up (first (last (network-interface-info
+                                                   (car iface)))))))
+            (network-interface-list))
+    t))
+
+;; On your first run, this should pull in all the base packages.
+(when (esk-online?) (ignore-errors (starter-kit-elpa-install)))
+
+(provide 'starter-kit-elpa)
