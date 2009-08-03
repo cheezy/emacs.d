35cd6b68f13581cee7e6df751a573a71fe9d4e9f
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2009-08-03 07:17:40 -0400

    added remember mode and org mode config

--- cheezy.el
+++ cheezy.el
@@ -167,6 +167,72 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
+(custom-set-variables
+ '(calendar-week-start-day 1)
+ '(org-agenda-files (quote ("~/gtd/mygtd.org")))
+ '(org-agenda-ndays 7)
+ '(org-agenda-repeating-timestamp-show-all nil)
+ '(org-agenda-restore-windows-after-quit t)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-sorting-strategy (quote ((agenda time-up priority-down tag-up) (todo tag-up))))
+ '(org-agenda-start-on-weekday nil)
+ '(org-agenda-todo-ignore-deadlines t)
+ '(org-agenda-todo-ignore-scheduled t)
+ '(org-agenda-todo-ignore-with-date t)
+ '(org-agenda-window-setup (quote other-window))
+ '(org-deadline-warning-days 7)
+ '(org-export-html-style "<link rel=\"stylesheet\" type=\"text/css\" href=\"mystyles.css\">")
+ '(org-fast-tag-selection-single-key nil)
+ '(org-log-done (quote (done)))
+ '(org-refile-targets (quote (("newgtd.org" :maxlevel . 1) ("someday.org" :level . 2))))
+ '(org-reverse-note-order nil)
+ '(org-tags-column -78)
+ '(org-tags-match-list-sublevels nil)
+ '(org-time-stamp-rounding-minutes 5)
+ '(org-use-fast-todo-selection t)
+ '(org-use-tag-inheritance nil))
+(setq org-agenda-custom-commands
+      '(
+        
+        ("P" "Projects"   
+         ((tags "PROJECT")))
+
+        ("H" "Office and Home Lists"
+         ((agenda)
+          (tags-todo "OFFICE")
+          (tags-todo "HOME")
+          (tags-todo "COMPUTER")
+          (tags-todo "DVD")
+          (tags-todo "READING")))
+
+        ("D" "Daily Action List"
+         (
+          (agenda "" ((org-agenda-ndays 1)
+                      (org-agenda-sorting-strategy
+                       (quote ((agenda time-up priority-down tag-up) )))
+                      (org-deadline-warning-days 0)
+                      ))))
+        ))
+
+
+;; Remember-mode
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/remember-2.0"))
+(autoload 'remember "remember" nil t)
+(autoload 'remember-region "remember" nil t)
+(define-key global-map "\C-cr" 'org-remember)
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+(define-key global-map "\C-cr" 'org-remember)
+(setq org-remember-templates
+      '(
+        ("Todo" ?t "* TODO %^{Brief Description} %^g\n%?\nAdded: %U" "~/gtd/mygtd.org" "Tasks")
+        ("Private" ?p "\n* %^{topic} %T \n%i%?\n" "~/gtd/mygtd.org")
+        ("WordofDay" ?w "\n* %^{topic} \n%i%?\n" "~/gtd/mygtd.org")
+        ))
+
 
 (require 'carbon-font)
 (prefer-coding-system 'utf-8)
