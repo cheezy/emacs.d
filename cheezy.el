754ff7edb3b38fe0ed0c14c704c7521136410de0
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2010-10-02 12:12:32 -0400

    fixed path so it grabs from shell

--- cheezy.el
+++ cheezy.el
@@ -1,13 +1,14 @@
 
 ;; CHEEZY settings
 
-(let ((path))
-  (setq path (concat "/opt/local/bin:"
-                     "~/bin:"
-                     "/usr/local/bin:"
-                     "/usr/bin"
-                     "/bin"))
-  (setenv "PATH" path))
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell 
+         (replace-regexp-in-string "[[:space:]\n]*$" "" 
+                                   (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
+
 
 (add-to-list 'load-path (concat dotfiles-dir "/vendor"))
 
