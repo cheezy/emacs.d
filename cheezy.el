b2d73eaeea6035f5173183c5cdf06adff3ac2b1f
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-07-21 11:14:19 -0400

    fixed typo in url

--- cheezy.el
+++ cheezy.el
@@ -13,7 +13,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.new/packages/")))
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (require 'cheezy/meta)
 
